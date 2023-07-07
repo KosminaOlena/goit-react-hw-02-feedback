fa603825b79ae252ef720b6aceeb5b4f9dc33da1
KosminaOlena <113665261+KosminaOlena@users.noreply.github.com>
2023-07-07 17:21:46 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,65 @@
-export const App = () => {
+import { Component } from 'react'
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions'
+import Statistics from './Statistics/Statistics'
+import Section from './Section/Section'
+import Notification from './Notification/Notification'
+import { Container } from './App.styled'
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  }
+  onLeaveFeedback = (key) =>{
+    this.setState((prevState) => ({
+      [key]: prevState[key] + 1
+    }));
+  }
+  countTotalFeedback = () => {
+      const { good, neutral, bad } = this.state;
+      return good + neutral + bad;
+
+  }
+  countPositiveFeedbackPercentage = () => {
+    const totalFeedback = this.countTotalFeedback();
+    if (totalFeedback === 0) {
+      return 0;
+    }
+    return Math.round((this.state.good / totalFeedback) * 100);
+  }
+  onStatisticsSection = () =>{
+    const totalFeedback = this.countTotalFeedback();
+    if (totalFeedback === 0) {
+      return (
+      <Notification 
+        message="There is no feedback"
+        />)
+    }
+    return(<Statistics
+    good={this.state.good}
+    neutral={this.state.neutral}
+    bad={this.state.bad}
+    total={this.countTotalFeedback}
+    positivePercentage={this.countPositiveFeedbackPercentage}
+    />)
+
+  }
+  render(){
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+    <Container>
+      <Section title = 'Please leave feedback'>
+      <FeedbackOptions 
+      options={this.state}
+      onLeaveFeedback={this.onLeaveFeedback}
+      />
+      </Section>
+
+      <Section title = 'Statistics'>
+        {this.onStatisticsSection()}
+      </Section>
+
+    </Container>
+  )}
+}
+export default App
